d100c21c290ac6e907e0e828e259fa6f5c91ffb7
CodeByAlex <wilsonc.alexander@gmail.com>
2022-06-29 14:58:54 -0400

    remove unnecessary files

--- src/preset/manager.ts
+++ src/preset/manager.ts
@@ -1,19 +1,10 @@
 import { addons, types } from "@storybook/addons";
 
 import { ADDON_ID, TOOL_ID, PANEL_ID, TAB_ID } from "../constants";
-import { Tool } from "../Tool";
 import { Panel } from "../Panel";
-import { Tab } from "../Tab";
 
 // Register the addon
 addons.register(ADDON_ID, () => {
-  // Register the tool
-  addons.add(TOOL_ID, {
-    type: types.TOOL,
-    title: "My addon",
-    match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story|docs)$/)),
-    render: Tool,
-  });
 
   // Register the panel
   addons.add(PANEL_ID, {
@@ -23,14 +14,4 @@ addons.register(ADDON_ID, () => {
     render: Panel,
   });
 
-  // Register the tab
-  addons.add(TAB_ID, {
-    type: types.TAB,
-    title: "My addon",
-    //👇 Checks the current route for the story
-    route: ({ storyId }) => `/myaddon/${storyId}`,
-    //👇 Shows the Tab UI element in myaddon view mode
-    match: ({ viewMode }) => viewMode === "myaddon",
-    render: Tab,
-  });
 });
