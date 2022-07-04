d6fd3eb180842d49787aae74f6e97e6e9c72debc
CodeByAlex <wilsonc.alexander@gmail.com>
2022-07-04 14:13:04 -0400

    read css props into panel

--- src/components/PanelContent.tsx
+++ src/components/PanelContent.tsx
@@ -1,9 +1,31 @@
 import React, { Fragment } from "react";
 import { styled, themes, convert } from "@storybook/theming";
 import { TabsState, Placeholder, Button } from "@storybook/components";
+import { CSS_PROPS } from '../css-props';
 
 export const PanelContent = () => (
   <div>
-    Hello World
+    {CSS_PROPS.map(obj => {
+      const propName = obj.name;
+      const propDesc = obj.description;
+      const propOptions: String[] = obj.values;
+      const propDefaultValue = obj.default;
+
+      return <div>
+        <div>Property Name: {propName}</div>
+        <div>Property Description: {propDesc}</div>
+        <div>Property Default Value: {propDefaultValue}</div>
+        {propOptions ?
+          <ul>
+            {propOptions.map(function (a) {
+              return (
+                <li>{a}</li>
+              );
+            })}
+          </ul> : null
+        }
+        <br />
+      </div>
+    })}
   </div>
 );
---
+++ src/css-props.ts
@@ -0,0 +1,556 @@
+export const CSS_PROPS = [
+    {
+        "name": "azimuth",
+        "description": "The position of a sound source along the horizontal axis of the listener's environment.",
+        "default": "",
+        "values": [
+            "angle",
+            "left-side",
+            "far-left",
+            "left",
+            "center-left",
+            "center",
+            "center-right",
+            "right",
+            "far-right",
+            "right-side",
+            "far-right",
+            "behind center"
+        ]
+    },
+    {
+        "name": "border-collapse",
+        "description": "Sets whether table borders should collapse into a single border or be separated as in standard HTML.",
+        "default": "separate",
+        "values": [
+            "separate",
+            "collapse",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "border-spacing",
+        "description": "Sets the distance between the borders of adjacent cells.",
+        "default": "2px",
+        "values": [
+            "0px",
+            "2px",
+            "5px",
+            "10px",
+            "15px",
+            "20px",
+            "25px",
+            "30px",
+            "35px",
+            "40px",
+            "45px",
+            "50px",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "caption-side",
+        "description": "Specifies the placement of a table caption.",
+        "default": "top",
+        "values": [
+            "top",
+            "bottom",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "color",
+        "description": "Specifies the color of text.",
+        "default": "inherit",
+        "values": [
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "indigo",
+            "violet",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "cursor",
+        "description": "Specifies the placement of a table caption.",
+        "default": "auto",
+        "values": [
+            "alias",
+            "all-scroll",
+            "auto",
+            "cell",
+            "context-menu",
+            "col-resize",
+            "copy",
+            "crosshair",
+            "default",
+            "e-resize",
+            "ew-resize",
+            "grab",
+            "grabbing",
+            "help",
+            "move",
+            "n-resize",
+            "ne-resize",
+            "nesw-resize",
+            "ns-resize",
+            "nw-resize",
+            "nwse-resize",
+            "no-drop",
+            "none",
+            "not-allowed",
+            "pointer",
+            "progress",
+            "row-resize",
+            "s-resize",
+            "se-resize",
+            "sw-resize",
+            "text",
+            "vertical-text",
+            "w-resize",
+            "wait",
+            "zoom-in",
+            "zoom-out",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "direction",
+        "description": "Specifies the text direction/writing direction within a block-level element.",
+        "default": "ltr",
+        "values": [
+            "ltr",
+            "rtl",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "elevation",
+        "description": "A spacial audio setting for aural content presentation that is used to specify the vertical direction a sound is coming from.",
+        "default": "level",
+        "values": [
+            "below",
+            "level",
+            "above",
+            "higher",
+            "lower",
+            "inherit"
+        ]
+    },
+    {
+        "name": "empty-cells",
+        "description": "Sets whether or not to display borders on empty cells in a table.",
+        "default": "show",
+        "values": [
+            "show",
+            "hide",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "font-family",
+        "description": "Specifies the font for an element.",
+        "default": "",
+        "values": [
+            "Arial",
+            "Times New Roman",
+            "Verdana",
+            "cursive"
+        ]
+    },
+    {
+        "name": "font-size",
+        "description": "Specifies the size of a font.",
+        "default": "medium",
+        "values": [
+            "medium",
+            "xx-small",
+            "x-small",
+            "small",
+            "large",
+            "x-large",
+            "xx-large",
+            "smaller",
+            "larger",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "font-style",
+        "description": "Specifies the font style for a text.",
+        "default": "normal",
+        "values": [
+            "normal",
+            "italic",
+            "oblique",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "font-variant",
+        "description": "Specifies whether or not a text should be displayed in a small-caps font.",
+        "default": "normal",
+        "values": [
+            "normal",
+            "small-caps",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "font-weight",
+        "description": "Sets how thick or thin characters in text should be displayed",
+        "default": "normal",
+        "values": [
+            "normal",
+            "bold",
+            "bolder",
+            "lighter",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "letter-spacing",
+        "description": "Increases or decreases the space between characters in a text.",
+        "default": "normal",
+        "values": [
+            "0px",
+            "2px",
+            "5px",
+            "10px",
+            "15px",
+            "20px",
+            "25px",
+            "30px",
+            "35px",
+            "40px",
+            "45px",
+            "50px",
+            "normal",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "line-height",
+        "description": "Specifies the height of a line.",
+        "default": "normal",
+        "values": [
+            "0px",
+            "2px",
+            "5px",
+            "10px",
+            "15px",
+            "20px",
+            "25px",
+            "30px",
+            "35px",
+            "40px",
+            "45px",
+            "50px",
+            "normal",
+            "initial",
+            "inherit"
+        ]
+    },
+    // {
+    //     "name": "list-style-image",
+    //     "description": "Replaces the list-item marker with an image.",
+    //     "values": []
+    // },
+    {
+        "name": "list-style-position",
+        "description": "Specifies the position of the list-item markers (bullet points).",
+        "default": "outside",
+        "values": [
+            "inside",
+            "outside",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "list-style-type",
+        "description": "Specifies the type of list-item marker in a list.",
+        "default": "disc",
+        "values": [
+            "disc",
+            "armenian",
+            "circle",
+            "cjk-ideographic",
+            "decimal",
+            "decimal-leading-zero",
+            "georgian",
+            "hebrew",
+            "hiragana",
+            "hiragana-iroha",
+            "katakana",
+            "katakana-iroha",
+            "lower-alpha",
+            "lower-greek",
+            "lower-latin",
+            "lower-roman",
+            "none",
+            "square",
+            "upper-alpha",
+            "upper-greek",
+            "upper-latin",
+            "upper-roman",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "pitch-range",
+        "description": "Defines the amount of variation permitted in the pitch of spoken text.",
+        "default": "",
+        "values": [
+            "10",
+            "20",
+            "30",
+            "40",
+            "50",
+            "60",
+            "70",
+            "80",
+            "90",
+            "100"
+        ]
+    },
+    {
+        "name": "pitch",
+        "description": "Specifies the average pitch of the speaking voice used to render spoken text.",
+        "default": "medium",
+        "values": [
+            "x-low",
+            "low",
+            "medium",
+            "high",
+            "x-high"
+        ]
+    },
+    {
+        "name": "quotes",
+        "description": "Sets the type of quotation marks for quotations.",
+        "default": "\"",
+        "values": [
+            "\"",
+            "'",
+            "‹",
+            "›",
+            "«",
+            "»",
+            "‘",
+            "’",
+            "“",
+            "”",
+            "„",
+            "none",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "richness",
+        "description": "specifies the richness, or brightness, of the speaking voice. Higher values of richness/brightness have rougher waveforms and tend to carry better than lower richness values with smoother waveforms.",
+        "default": "initial",
+        "values": [
+            "0",
+            "10",
+            "20",
+            "30",
+            "40",
+            "50",
+            "60",
+            "70",
+            "80",
+            "90",
+            "100",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "speak-header",
+        "description": "Used in aural media to specify whether table headers are spoken before each cell, or only before each cell if that cell uses a different header to the previous cell.",
+        "default": "once",
+        "values": [
+            "once",
+            "always",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "speak-numeral",
+        "description": "Used in aural media to specify how to speak numerals (numbers).",
+        "default": "digits",
+        "values": [
+            "digits",
+            "continuous",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "speak-punctuation",
+        "description": "Used in aural media to specify how punctuation (i.e. comma) should be spoken.",
+        "default": "none",
+        "values": [
+            "code",
+            "none",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "speech-rate",
+        "description": "Used to declare the rate at which text is spoken.",
+        "default": "inherit",
+        "values": [
+            "number",
+            "x-slow",
+            "slow",
+            "medium",
+            "fast",
+            "x-fast",
+            "faster",
+            "slower",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "stress",
+        "description": "Specifies the amount of inflection which is used to speak stress markers in a language.",
+        "default": "inherit",
+        "values": [
+            "0",
+            "10",
+            "20",
+            "30",
+            "40",
+            "50",
+            "60",
+            "70",
+            "80",
+            "90",
+            "100",
+            "inherit"
+        ]
+    },
+    {
+        "name": "text-align",
+        "description": "Specifies the horizontal alignment of text in an element.",
+        "default": "left",
+        "values": [
+            "left",
+            "right",
+            "center",
+            "justify",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "text-indent",
+        "description": "Specifies the indentation of the first line in a text-block.",
+        "default": "0px",
+        "values": [
+            "0px",
+            "10px",
+            "20px",
+            "30px",
+            "40px",
+            "50px",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "text-transform",
+        "description": "Controls the capitalization of text.",
+        "default": "none",
+        "values": [
+            "none",
+            "capitalize",
+            "uppercase",
+            "lowercase",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "visibility",
+        "description": "Specifies whether or not an element is visible.",
+        "default": "visible",
+        "values": [
+            "visible",
+            "hidden",
+            "collapse",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "volume",
+        "description": "Specifies the current volume of the audio/video",
+        "default": "1.0",
+        "values": [
+            "0.0",
+            "0.1",
+            "0.2",
+            "0.3",
+            "0.4",
+            "0.5",
+            "0.6",
+            "0.7",
+            "0.8",
+            "0.9",
+            "1.0"
+        ]
+    },
+    {
+        "name": "white-space",
+        "description": "Specifies how white-space inside an element is handled.",
+        "default": "normal",
+        "values": [
+            "normal",
+            "nowrap",
+            "pre",
+            "pre-line",
+            "pre-wrap",
+            "initial",
+            "inherit"
+        ]
+    },
+    {
+        "name": "word-spacing",
+        "description": "Increases or decreases the white space between words.",
+        "default": "normal",
+        "values": [
+            "normal",
+            "0px",
+            "2px",
+            "4px",
+            "6px",
+            "8px",
+            "10px",
+            "initial",
+            "inherit"
+        ]
+    }
+]
