35eacc7ce561a50c8713aff0deb40a2966d1a9e6
CodeByAlex <wilsonc.alexander@gmail.com>
2022-07-04 16:41:48 -0400

    add random picker functionality

--- src/components/PanelContent.tsx
+++ src/components/PanelContent.tsx
@@ -5,35 +5,42 @@ import { CSS_PROPS } from '../css-props';
 
 
 const randomizePropValues = () => {
-  console.log('randomizePropValues')
+  const formElement = document.querySelector("form");
+  for(const el of Array.from(formElement.elements)) {
+    const options = el.children;  
+    const random  = Math.floor(Math.random() * options.length);
+    (el as any).value = (options[random] as any).value; 
+  }
 }
 
 export const PanelContent = () => (
-  <div style={{ margin: '16px'}}>
-    <Button primary small style={{ position: 'sticky', top: 16, zIndex: 100, float: 'right'}} onClick={randomizePropValues}>Randomize!</Button>
-    <div>
-      {CSS_PROPS.sort((a, b) => a.name.localeCompare(b.name)).map(obj => {
-        const propName = obj.name;
-        const propDesc = obj.description;
-        const propOptions: String[] = obj.values;
-        const propDefaultValue = obj.default;
+  <div style={{ margin: '16px' }}>
+    <Button primary small style={{ position: 'sticky', top: 16, zIndex: 100, float: 'right' }} onClick={randomizePropValues}>Randomize!</Button>
+    <form>
+      <div>
+        {CSS_PROPS.sort((a, b) => a.name.localeCompare(b.name)).map(obj => {
+          const propName = obj.name;
+          const propDesc = obj.description;
+          const propOptions: String[] = obj.values;
+          const propDefaultValue = obj.default;
 
-        return <div style={{ marginBottom: '16px'}}>
-          <h3>Name: {propName}</h3>
-          <div>Description: {propDesc}</div>
-          {propOptions ?
-            <Form.Select style={{ marginTop: '8px'}} defaultValue={propDefaultValue}>
-              {propOptions.map(function (a) {
-                return (
-                  <option>{a}</option>
-                );
-              })}
-            </Form.Select>
-            : null
-          }
-        </div>
-      })}
-    </div>
+          return <div style={{ marginBottom: '16px' }}>
+            <h3>Name: {propName}</h3>
+            <div>Description: {propDesc}</div>
+            {propOptions ?
+              <Form.Select style={{ marginTop: '8px' }} defaultValue={propDefaultValue}>
+                {propOptions.map(function (a) {
+                  return (
+                    <option>{a}</option>
+                  );
+                })}
+              </Form.Select>
+              : null
+            }
+          </div>
+        })}
+      </div>
+    </form>
   </div >
 );
 
