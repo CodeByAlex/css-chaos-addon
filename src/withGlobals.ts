89e93efdc4b012087014f49dbd24bdd68d4aba82
CodeByAlex <wilsonc.alexander@gmail.com>
2022-07-05 13:18:17 -0400

    removed withGlobals logic

--- src/withGlobals.ts
+++ src/withGlobals.ts
@@ -23,22 +23,22 @@ export const withGlobals: DecoratorFunction = (StoryFn, context) => {
 };
 
 function displayToolState(selector: string, state: any) {
-  const rootElement = document.querySelector(selector);
-  let preElement = rootElement.querySelector("pre");
+//   const rootElement = document.querySelector(selector);
+//   let preElement = rootElement.querySelector("pre");
 
-  if (!preElement) {
-    preElement = document.createElement("pre");
-    preElement.style.setProperty("margin-top", "2rem");
-    preElement.style.setProperty("padding", "1rem");
-    preElement.style.setProperty("background-color", "#eee");
-    preElement.style.setProperty("border-radius", "3px");
-    preElement.style.setProperty("max-width", "600px");
-    rootElement.appendChild(preElement);
-  }
+//   if (!preElement) {
+//     preElement = document.createElement("pre");
+//     preElement.style.setProperty("margin-top", "2rem");
+//     preElement.style.setProperty("padding", "1rem");
+//     preElement.style.setProperty("background-color", "#eee");
+//     preElement.style.setProperty("border-radius", "3px");
+//     preElement.style.setProperty("max-width", "600px");
+//     rootElement.appendChild(preElement);
+//   }
 
-  preElement.innerText = `This snippet is injected by the withGlobals decorator.
-It updates as the user interacts with the ⚡ tool in the toolbar above.
+//   preElement.innerText = `This snippet is injected by the withGlobals decorator.
+// It updates as the user interacts with the ⚡ tool in the toolbar above.
 
-${JSON.stringify(state, null, 2)}
-`;
+// ${JSON.stringify(state, null, 2)}
+// `;
 }
