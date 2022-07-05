cbed54fe72efda066368b933f539e24c6bc3fb14
CodeByAlex <wilsonc.alexander@gmail.com>
2022-07-04 23:35:10 -0400

    added updateCSSProps function

--- src/components/PanelContent.tsx
+++ src/components/PanelContent.tsx
@@ -4,13 +4,34 @@ import { Placeholder, Button, Form } from "@storybook/components";
 import { CSS_PROPS } from '../css-props';
 
 
+const getIframeDoc = () => {
+  const iframe = document.querySelector<HTMLIFrameElement>(
+    "#storybook-preview-iframe"
+  );
+  return iframe && iframe.contentWindow && iframe.contentWindow.document ? iframe.contentWindow.document : null;
+}
+
 const getForm = () => {
   return document.querySelector("form");
 }
 
-const updateForm = (event: any) => {
+const updateCSSProps = (propObj: any) => {
+  const iframeDoc = getIframeDoc();
+  if (iframeDoc && propObj) {
+    const root = iframeDoc.documentElement;
+    if (root) {
+      root.style.setProperty(`--${propObj.name}`, propObj.value);
+      console.log('set', `--${propObj.name}`)
+    }
+    console.log(root.style)
+  }
+}
+
+const formUpdated = (event: any) => {
   console.log(event)
   //change the existing css var for property type
+  updateCSSProps({ name: event.target.id, value: event.target.value } as any);
+
 }
 
 const randomizePropValues = () => {
@@ -20,38 +41,50 @@ const randomizePropValues = () => {
     const random = Math.floor(Math.random() * options.length);
     (el as any).value = (options[random] as any).value;
     //change the existing css var for property type
+    updateCSSProps({ name: el.id, value: (options[random] as any).value } as any);
   }
 }
 
-export const PanelContent = () => (
-  <div style={{ margin: '16px' }}>
-    <Button primary small style={{ position: 'sticky', top: 16, zIndex: 100, float: 'right' }} onClick={randomizePropValues}>Randomize!</Button>
-    <form>
-      <div>
-        {CSS_PROPS.sort((a, b) => a.name.localeCompare(b.name)).map(obj => {
-          const propName = obj.name;
-          const propDesc = obj.description;
-          const propOptions: String[] = obj.values;
-          const propDefaultValue = obj.default;
+const initCSSProps = () => {
+  CSS_PROPS.forEach((obj) => {
+    if (obj && obj.name && obj.default) {
+      updateCSSProps({ name: obj.name, value: obj.default } as any);
+    }
+  })
+}
 
-          return <div style={{ marginBottom: '16px' }}>
-            <h3>Name: {propName}</h3>
-            <div>Description: {propDesc}</div>
-            {propOptions ?
-              <Form.Select style={{ marginTop: '8px' }} defaultValue={propDefaultValue} onChange={updateForm}>
-                {propOptions.map((value) => {
-                  return (
-                    <option>{value}</option>
-                  );
-                })}
-              </Form.Select>
-              : null
-            }
-          </div>
-        })}
-      </div>
-    </form>
-  </div >
-);
+export const PanelContent = () => {
+  initCSSProps()
+  return (
+    <div style={{ margin: '16px' }}>
+      <Button primary small style={{ position: 'sticky', top: 16, zIndex: 100, float: 'right' }} onClick={randomizePropValues}>Randomize!</Button>
+      <form>
+        <div>
+          {CSS_PROPS.sort((a, b) => a.name.localeCompare(b.name)).map(obj => {
+            const propName = obj.name;
+            const propDesc = obj.description;
+            const propOptions: String[] = obj.values;
+            const propDefaultValue = obj.default;
+
+            return <div style={{ marginBottom: '16px' }}>
+              <h3>Name: {propName}</h3>
+              <div>Description: {propDesc}</div>
+              {propOptions ?
+                <Form.Select id={propName} style={{ marginTop: '8px' }} defaultValue={propDefaultValue} onChange={formUpdated}>
+                  {propOptions.map((value) => {
+                    return (
+                      <option>{value}</option>
+                    );
+                  })}
+                </Form.Select>
+                : null
+              }
+            </div>
+          })}
+        </div>
+      </form>
+    </div>
+  )
+};
 
 // Form usage: https://github.com/storybookjs/storybook/blob/main/lib/components/src/form/form.stories.tsx
